670495313ce77625e44695ff04056ec6ecd59b5c
Daniel de Brito <danieldebrito@outlook.com>
2020-11-26 19:42:49 -0300

    peliculas alta

--- src/app/peliculas/class/pelicula.ts
+++ src/app/peliculas/class/pelicula.ts
@@ -1,3 +1,6 @@
+import { Actor } from 'src/app/actores/class/actor';
+import { Pais } from 'src/app/paises/class/pais';
+
 export class Pelicula {
     constructor(
         public id?: string,
@@ -5,5 +8,7 @@ export class Pelicula {
         public genero?: string,
         public fechaestreno?: string,
         public foto?: string,
+        public actores?: Actor[],
+        public pais?: Pais
     ) { }
 }
--- src/app/peliculas/peliculas-board/peliculas-alta/peliculas-alta.component.ts
+++ src/app/peliculas/peliculas-board/peliculas-alta/peliculas-alta.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { PeliculasFirebaseService } from 'src/app/peliculas/services/peliculas-firebase.service';
 import { Pelicula } from 'src/app/peliculas/class/pelicula';
 
+import { ActoresFirebaseService } from 'src/app/actores/services/actores-firebase.service';
+import { Actor } from 'src/app/actores/class/actor';
+
+import { Pais } from 'src/app/paises/class/pais';
+import { PaisesService } from 'src/app/paises/services/paises.service';
+import { element } from 'protractor';
+
+
 @Component({
   selector: 'app-peliculas-alta',
   templateUrl: './peliculas-alta.component.html',
@@ -13,20 +21,53 @@ export class PeliculasAltaComponent implements OnInit {
   public pelicula: Pelicula = {};
   public paisNombre = '';
 
+  public actores: Actor[] = [];
+  public actoresAdd: Actor[] = [];
+  public actor: Actor = {};
+
+  public paises: Pais[] = [];
+  public pais: Pais = {};
+
   constructor(
     private peliculasService: PeliculasFirebaseService,
+    private actoresSvc: ActoresFirebaseService,
+    private paisesService: PaisesService,
   ) { }
-
-
 
   public add() {
     this.pelicula.foto = '/assets/killbill.jpg';
+    this.pelicula.actores = this.actoresAdd;
+    this.pelicula.pais = this.pais;
     this.peliculasService.addItem(this.pelicula);
     this.pelicula = {};
+    this.actoresAdd = [];
+  }
+
+  getActores() {
+    this.actoresSvc.getItems().subscribe(response => {
+      this.actores = response;
+      return response;
+    });
+  }
+
+  public agregarActor(event) {
+    this.actoresAdd.push(event.actorLanzado);
+    console.log(this.actoresAdd);
+  }
+
+  public agregarPais(event){
+    this.pais = event.paisEnviado;
+  }
+
+  public getPaises() {
+    this.paisesService.ListarO().subscribe(response => {
+        this.paises = response;
+    });
   }
 
   ngOnInit() {
-    // this.getPaises();
+    this.getActores();
+    this.getPaises();
   }
 }
 
